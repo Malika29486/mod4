9d425a4aea846b71ad3a5578360fcf5420e41152
Малика <Singlegirl2008@yandex.ru>
2023-09-23 16:49:25 +0700

    Комит 2

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -78,10 +78,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'hom': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'hom.postgresql',
     }
 }
 
