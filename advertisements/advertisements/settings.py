0cada29167a5cef6979fd24eb727030b2628f821
Малика <Singlegirl2008@yandex.ru>
2023-09-23 14:28:40 +0700

    Комит 4задание 2 урок

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'hom': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'hom.postgresql',
     }
 }
 
