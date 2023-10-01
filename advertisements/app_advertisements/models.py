7f08ea71c9ca8ba11331db9ba12a45d141773cc4
Малика <Singlegirl2008@yandex.ru>
2023-10-01 15:23:41 +0700

    Комит 4

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
 class Advertisement(models.Model):
     title = models.CharField("заголовок", max_length= 128)
@@ -8,8 +10,17 @@ class Advertisement(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    @admin.display(description="Дата создания")
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date()==timezone.now().date():
+            created_time=self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color:green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
     class Meta:
-        db_table="advertisement"
+        db_table="advertisements"
 
     def __str__(self):
-        return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+        return f"Advertisements(id={self.id}, title={self.title}, price={self.price})"
