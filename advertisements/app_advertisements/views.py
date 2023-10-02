cd3833e8c9652b88fe16b6ae484acb9d043389f5
Малика <Singlegirl2008@yandex.ru>
2023-10-02 19:54:17 +0700

    комит 6

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.http import HttpResponse
 from .models import Advertisement
+from .forms import AdvertisementForm
 
 def index(request):
     advertisements= Advertisement.objects.all()
@@ -9,4 +11,25 @@ def index(request):
 
 def top_sellers(request):
     return render(request, 'top-sellers.html' )
+
+def advertisement_post(request):
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement= Advertisement(**form.cleaned_data)
+            advertisement.user=request.user
+            advertisement.save()
+            url= reverse('main-page')
+            return redirect(url)
+    else:
+        form=AdvertisementForm()
+    context={'form':form}
+    return render(request, 'advertisement-post.html', context)
+
+    form = AdvertisementForm()
+    context = {'form': form}
+    return  render(request, 'advertisement-post.html', context)
+
+
+
 
